0f6d110b1f76084161273db2a35dbd7e41e71729
Quincy <qle@ciena.com>
2016-02-24 14:41:34 -0500

    porting code

--- tests/dummy/app/pods/demo/controller.js
+++ tests/dummy/app/pods/demo/controller.js
@@ -1,4 +1,21 @@
-import Ember from 'ember'
+import Ember from "ember"
 
 export default Ember.Controller.extend({
-})
+  errored: true,
+
+  actions: {
+    text(attrs) {
+      this.notifications.addNotification({
+        message: "value: '" + attrs.value + "'",
+        type: "success",
+        autoClear: true,
+        clearDuration: 2000
+      });
+    },
+
+    toggleError() {
+      this.toggleProperty('error')
+      this.toggleProperty('errored')
+    }
+  }
+});
